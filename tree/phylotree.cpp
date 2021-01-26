b2d3f4cfe01358f224eb4b465dd053a0b42076f4
tycheyoung <retbird13@gmail.com>
2021-01-26 20:41:20 +0000

    use score as-is

--- tree/phylotree.cpp
+++ tree/phylotree.cpp
@@ -1285,7 +1285,7 @@ double PhyloTree::computeLikelihoodGPU() {
     elem_t score = 0.0;
     cuda_maxll_score(score, seq_transpose_concat, treeArray, treeLengthArray, nodeLevel, rate_mat, 
                      pi, nodeNum, seq_length, seq_num);
-    return -score;
+    return score;
 
 }
 
